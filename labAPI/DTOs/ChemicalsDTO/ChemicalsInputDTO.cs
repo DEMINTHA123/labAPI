6d243d9a1b34cc160610096260fedb11ab51f70b
DEMINTHA123 <deminthasg@gmail.com>
2023-02-17 19:03:23 +0530

    Error Corrections

--- labAPI/DTOs/ChemicalsDTO/ChemicalsInputDTO.cs
+++ labAPI/DTOs/ChemicalsDTO/ChemicalsInputDTO.cs
@@ -6,8 +6,6 @@
         public string? Name { get; set; }
         public string? Symbol { get; set; }
         public string? MatterState { get; set; }
-        public string? Construction
-        {
-            get; set;
-        }
+        public string? Construction { get; set; }
+    }
 }
